23c28e5c5789d2298c29db334ad2df992f72b429
Boris A. Burkov <vasjaforutube@gmail.com>
2017-03-02 20:21:37 +0000

    First E2E tests with protractor.

--- test/e2e/genoverse.spec.js
+++ test/e2e/genoverse.spec.js
@@ -1,6 +1,49 @@
 describe("genoverse directive spec", function() {
-    it("should update start and end location in response to browser viewport movement", function() {
+    var Homepage = function() {
         browser.get("http://localhost:8000/index.html");
 
+        this.genomicStartInput = element(by.css('.genomic-start-input'));
+        this.genomicEndInput = element(by.css('.gnomic-end-input'));
+
+        this.gvScrollRightButton = element(by.css('.gv-scroll-right'));
+        this.gvWrapper = element(by.css('.gv-wrapper'));
+    };
+
+    var page;
+
+    beforeEach(function() {
+        page = new Homepage()
     });
+
+    it("should update start and end locations in response to viewport movement button", function() {
+        var initialStart = page.genomicStartInput.getAttribute('value');
+        var initialEnd = page.genomicEndInput.getAttribute('value');
+
+        page.gvScrollRightButton.click();
+
+        var updatedStart = page.genomicStartInput.getAttribute('value');
+        var updatedEnd = page.genomicEndInput.getAttribute('value');
+        
+        expect(updatedStart > initialStart).toBe(true);
+        expect(updatedEnd > initialEnd).toBe(true)
+    });
+
+    it("should update start and end locations in response to viewport drag", function() {
+        var initialStart = page.genomicStartInput.getAttribute('value');
+        var initialEnd = page.genomicEndInput.getAttribute('value');
+
+        // drag canvas, increase coordinates
+        browser.action()
+            .mouseMove(this.gvWrapper, {x: 200, y: 100})
+            .mouseDown()
+            .mouseMove({x: 100, y: 100})
+            .mouseUp()
+            .perform();
+
+        var updatedStart = page.genomicStartInput.getAttribute('value');
+        var updatedEnd = page.genomicEndInput.getAttribute('value');
+
+        expect(updatedStart > initialStart).toBe(true);
+        expect(updatedEnd > initialEnd).toBe(true)
+    })
 });
